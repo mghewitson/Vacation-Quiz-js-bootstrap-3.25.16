c1ef8741d6d3eba194f42964479413362315689a
Miles Hewitson <mghewitson@gmail.com>
2016-03-25 16:48:45 -0700

    adds readme and additional styling to form sizes, and result images

---
+++ README.md
@@ -0,0 +1,24 @@
+# _Miles Hewitson_
+
+#### _Vacation Quiz Utilizing jS Branching_
+
+#### By _**Miles Hewitson**_
+
+## Description
+
+_This is a quiz that assigns a numeric value to each user response, and with the power of jS branching will display different results depending on the combination of user inputs._
+
+## Support and contact details
+
+_If you have any issues with the site or notice any bugs, please contact me at:_
+_mghewitson@gmail.com_
+
+## Technologies Used
+
+_The technologies used were CSS and Bootstrap for page styling purposes. JavaScript and jQuery were used to both tabulate the results and trigger the showing of the result to the user._
+
+### License
+
+*This software is licensed under the MIT license*
+
+Copyright (c) 2016 **_Miles Hewitson_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     var points = 0;
-    // var point = parseInt($("option").val());
     var result = parseInt($("#gender").val());
     points += result;
-
     result = parseInt($("#candy").val());
     points += result;
-
     result = parseInt($("#future").val());
     points += result;
-
     result = parseInt($("#pres").val());
     points += result;
-
     result = parseInt($("input:radio[name=country]:checked").val());
     points += result;
 
     if (points === 5){
-      $("#answer1").show();
+      $("#answer1").slideDown(1750);
 }
-    if (points > 5 && points < 11){
-      $("#answer2").show();
+    if (points > 5 && points < 12){
+      $("#answer2").slideDown(1750);
 }
-    if (points > 11 && points < 15){
-      $("#answer3").show();
+    if (points > 11 && points < 16){
+      $("#answer3").slideDown(1750);
 }
     if (points > 15 && points < 20){
-      $("#answer4").show();
+      $("#answer4").slideDown(1750);
 }
     event.preventDefault();
   });
